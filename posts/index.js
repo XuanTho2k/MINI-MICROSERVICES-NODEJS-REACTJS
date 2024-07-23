f85dd59899ed5c79ed9cf3161b7ae0ff00e28665
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-23 14:20:42 +0700

    feature/event-bus

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { randomBytes } from "crypto";
+import axios from "axios";
 
 const app = express();
 import bodyParser from "body-parser";
@@ -8,10 +9,11 @@ const posts = {};
 
 app.use(bodyParser.json());
 app.use(cors());
+
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
 
   const { title } = req.body;
@@ -20,9 +22,22 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios.post("http://localhost:8080/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
 });
 
-app.listen(4001, () => {
-  console.log("ok");
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
+});
+app.listen(4000, () => {
+  console.log("Post service is running on " + 4000);
 });
