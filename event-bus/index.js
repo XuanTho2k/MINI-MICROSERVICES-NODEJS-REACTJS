d87f9005dff70aedb258762ec5cbaa8126cdb27f
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-24 15:17:22 +0700

    feature[events-array]

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import cors from "cors";
 
+const events = [];
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/events", (req, res) => {
   const event = req.body;
   console.log(event.type);
+
+  events.push(event);
 
   // post service
   axios
@@ -27,6 +32,10 @@ app.post("/events", (req, res) => {
     .catch((err) => console.log(err.message));
 
   res.send({ status: "ok" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(8080, () => {
